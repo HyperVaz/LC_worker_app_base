f199a40f9f429c08cac84e4e459478497943750b
HyperVaz <timvas14@gmail.com>
2025-04-21 19:54:02 +0300

    Ðadd controller, models and migrations

---
+++ app/Http/Controllers/WorkerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WorkerController extends Controller
+{
+    public function index(){
+        return 'work index';
+    }
+
+    public function show(){
+        return 'work show';
+    }
+}
